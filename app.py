a0ba0d1bac40cd90c28cf77b033c5b1c100e89c7
Ilia Gilmijarow <gilmijar@gmail.com>
2022-07-21 00:59:05 +0200

    further development, able to add user to users table

--- app.py
+++ app.py
@@ -1,13 +1,59 @@
-from flask import Flask
-from flask import render_template, redirect, url_for, request
-from flask import session
 from os import urandom, environ
+from flask import Flask, g, render_template, redirect, url_for, request, abort  # , session
+import sqlite3
+from argon2 import PasswordHasher
+
+ph = PasswordHasher()
+
 
 app = Flask(__name__)
 if environ.get('FLASK_ENV') == 'development':
     app.secret_key = b'Hakuna Matata \n\n\t'
 else:
-    app.secret_key = urandom(64)
+    app.secret_key = urandom(64).hex()
+DATABASE = 'db.db'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(_):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def get_user(email):
+    sql = '''
+        SELECT * FROM users WHERE email = ?
+    '''
+    db = get_db()
+    cur = db.execute(sql, (email,))
+    return cur.fetchone()
+
+
+def create_user(user_data):
+    if get_user(user_data['email']):
+        return False
+    sql = '''
+        INSERT INTO users
+        (first_name, last_name, pass, email, phone, "role")
+        VALUES(:first_name, :last_name, :hashed_pass, :email, :phone, 'student');
+    '''
+    user_data['hashed_pass'] = ph.hash(user_data['pass'])
+    db = get_db()
+    db.execute(sql, user_data)
+    db.commit()
+    user = get_user(user_data['email'])
+    return user
+
+# TODO: write remove_user
 
 
 @app.route('/')
@@ -24,5 +70,19 @@ def login():
 
 @app.post('/register')
 def register():
-    request.form.get('email', None)
-    return "<h1>not implemented yet</h1>"
+    # in field_mapping keys are for pyton code, values are form field names.
+    field_mapping = {
+        'email': 'login',
+        'first_name': 'first_name',
+        'last_name': 'last_name',
+        'phone': 'phone',
+        'pass': 'password'
+    }
+    # TODO: validate activation code
+    if request.form.get('password', None) != request.form.get('repeat-password', ...):
+        abort(405, 'passwords must match!')
+    user_data = {k: request.form.get(v, None) for k, v in field_mapping.items()}
+    user_record = create_user(user_data)
+    print(*user_record, sep='\n')
+    return repr(user_record)
+
