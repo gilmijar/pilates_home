2f2f99e872356770642c43b252b731f612e0fed7
Ilia Gilmijarow <gilmijar@gmail.com>
2022-08-12 00:21:14 +0200

    added remove user function, added generic execute DML function

--- app.py
+++ app.py
@@ -45,15 +45,41 @@ def create_user(user_data):
         INSERT INTO users
         (first_name, last_name, pass, email, phone, "role")
         VALUES(:first_name, :last_name, :hashed_pass, :email, :phone, 'student');
-    '''
+        '''
     user_data['hashed_pass'] = ph.hash(user_data['pass'])
-    db = get_db()
-    db.execute(sql, user_data)
-    db.commit()
+    perform_dml(sql, user_data)
     user = get_user(user_data['email'])
     return user
 
-# TODO: write remove_user
+
+def remove_user(who):
+    sql = '''
+        DELETE FROM {table} WHERE user_id = ?
+        '''
+    tables = ('user_class', 'user_group', 'allowed_user_group', 'users')
+    deletes = {k: 0 for k in tables}
+    for table in tables:
+        affected_rows = perform_dml(sql.format(table=table), [who])
+        deletes[table] = affected_rows
+    return deletes
+
+
+def perform_dml(dml_statement, params=None):
+    db = get_db()
+    cur = db.cursor()
+    if not params:
+        cur.execute(dml_statement)
+    elif isinstance(params, list):
+        if all(isinstance(param, (list, tuple)) and len(param) == 2 for param in params):
+            cur.executemany(dml_statement, params)
+        elif all(isinstance(param, (str, int, float))for param in params):
+            cur.execute(dml_statement, params)
+        else:
+            raise ValueError('One of parameters for the query is invalid.')
+    else:
+        raise ValueError('Invalid type supplied for query as parameter list')
+    db.commit()
+    return cur.rowcount
 
 
 @app.route('/')
